e111a231626bf9de36255aadc318ae27821b2385
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-10-21 19:05:37 +0100

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
-import Button1 from "./components/button-1"
+import React, { Component } from "react";
+import "./App.css";
+import Button1 from "./components/button-1";
+import Button2 from "./components/button-2";
+import Button3 from "./components/button-3";
 
 class App extends Component {
   render() {
     return (
-      <div><Button1/></div>
+      <div>
+        <Button1 />
+        <Button2 />
+        <Button3 />
+      </div>
     );
   }
 }
